81628c9e63bc30afb8377398a6be6dfa8b839d61
rampion19 <princegupta.pg19@gmail.com>
2022-12-05 13:17:00 +0530

    adding dom2 to git

---
+++ dom2.js
@@ -0,0 +1,9 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[2].textContent = 'Hello 2';
+//items[2].style.fontWeight = 'bold';
+items[2].style.background = 'green';
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
