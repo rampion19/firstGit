ce7e531c3a806e7d1a05f85ba58ba0f4fc9311d7
rampion19 <119778595+rampion19@users.noreply.github.com>
2022-12-06 14:56:14 +0530

    Revert "adding file to git of queryselector"

--- dom3.js
+++ dom3.js
@@ -1,17 +1,19 @@
-
-// var secondItem = document.querySelector('.list-group-item:nth-child(2');
-// var thirdItem = document.querySelector('.list-group-item:nth-child(3');
-
-// secondItem.style.color = "green";
-// thirdItem.style.display = "none";
-
-var items = document.querySelectorAll('.list-group-item');
-// items[1].style.color = 'green';
-
-for(var i=0; i<items.length; i+2){
-    items[0].style.background = 'green';
+var li = document.getElementsByClassName('list-group-item');
+console.log(li);
+console.log(li[1]);
+li[2].textContent = 'Hello 2';
+//items[2].style.fontWeight = 'bold';
+li[2].style.background = 'green';
+for(var i=0; i<li.length; i++){
+    li[i].style.fontWeight = 'bold';
 }
 
-
-
-
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[2].textContent = 'Hello 2';
+//items[2].style.fontWeight = 'bold';
+items[2].style.background = 'green';
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
